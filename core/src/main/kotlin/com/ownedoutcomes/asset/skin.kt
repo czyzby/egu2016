faa8030b138160246063fde8ee6cec5a29a30a36
Krzysiek Ruczaj <krj.public@gmail.com>
2016-11-19 12:02:53 +0100

    [UI] changed default font to font-export

--- core/src/main/kotlin/com/ownedoutcomes/asset/skin.kt
+++ core/src/main/kotlin/com/ownedoutcomes/asset/skin.kt
@@ -1,5 +1,6 @@
 package com.ownedoutcomes.asset
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.TextureAtlas
 import ktx.assets.loadOnDemand
@@ -8,7 +9,7 @@ import ktx.style.textButton
 
 fun loadSkin() = skin(atlas = loadOnDemand<TextureAtlas>(path = "skin.atlas").asset) {
     textButton("play-button") {
-        font = BitmapFont()
+        font = BitmapFont(Gdx.files.internal("font-export.fnt"), getRegion("font-export"), false)
         up = getDrawable("button")
         over = getDrawable("button-pressed")
         down = getDrawable("button-pressed")
