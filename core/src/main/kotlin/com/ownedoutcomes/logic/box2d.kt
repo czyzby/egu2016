b61fa0a720b0818bf1c43eff16df7aaaa1453378
MJ <john.hervicc@gmail.com>
2016-11-19 13:28:38 +0100

    Added player movement.

--- core/src/main/kotlin/com/ownedoutcomes/logic/box2d.kt
+++ core/src/main/kotlin/com/ownedoutcomes/logic/box2d.kt
@@ -1,14 +1,18 @@
 package com.ownedoutcomes.logic
 
+import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.physics.box2d.*
 import com.badlogic.gdx.utils.viewport.FitViewport
 import com.badlogic.gdx.utils.viewport.Viewport
+import com.ownedoutcomes.logic.entity.Food
 import com.ownedoutcomes.logic.entity.Player
 import ktx.collections.gdxArrayOf
 import ktx.math.vec2
 
 val gameWorldWidth = 8f
 val gameWorldHeight = 6f
+val halfGameWorldWidth = gameWorldWidth / 2f
+val halfGameWorldHeight = gameWorldHeight / 2f
 
 class GameController {
     private val gameViewport: Viewport = FitViewport(gameWorldWidth, gameWorldHeight)
@@ -16,6 +20,9 @@ class GameController {
     private val inputController = InputController(gameViewport)
     private lateinit var world: World
     val players = gdxArrayOf<Player>()
+    private val food = gdxArrayOf<Food>()
+    private var timeSinceSpawn = 100f
+
     fun reload() {
         world = World(vec2(0f, 0f), true)
         addBodies()
@@ -30,10 +37,18 @@ class GameController {
     }
 
     fun update(delta: Float) {
+        timeSinceSpawn += delta
+        if (timeSinceSpawn > MathUtils.random(2f, 4f)) {
+            food.add(Food(world))
+            timeSinceSpawn = 0f
+        }
         inputController.update()
         world.step(delta, 8, 3)
         players.forEach { it.update(delta) }
+        food.forEach { it.update(delta) }
         // TODO remove destroyed bodies
+        // TODO add world bounds
+        // TODO remove enemies that touch world bounds
         renderer.render(world, gameViewport.camera.combined)
     }
 }
--- core/src/main/kotlin/com/ownedoutcomes/logic/entity/player.kt
+++ core/src/main/kotlin/com/ownedoutcomes/logic/entity/player.kt
@@ -4,11 +4,13 @@ import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.physics.box2d.*
 import com.ownedoutcomes.logic.InputController
 
+val playerDensity = 20f
+
 class Player(world: World, val inputController: InputController) : AbstractEntity(world) {
-    var size: Int = 1
+    var size: Float = 0.2f
     override fun createBody(world: World): Body {
         val circle = CircleShape()
-        circle.radius = 0.5f
+        circle.radius = 0.2f
         val body = BodyDef().apply {
             type = BodyDef.BodyType.DynamicBody
             fixedRotation = true
@@ -16,7 +18,7 @@ class Player(world: World, val inputController: InputController) : AbstractEntit
         }
         val fixture = FixtureDef().apply {
             shape = circle
-            density = 20f
+            density = playerDensity
             friction = 0.3f
             restitution = 0.1f
         }
@@ -26,15 +28,25 @@ class Player(world: World, val inputController: InputController) : AbstractEntit
     }
 
     override fun update(delta: Float) {
+        val currentDensity = 10f + size * size * MathUtils.PI * playerDensity
+        body.applyForceToCenter(
+                -body.linearVelocity.x * currentDensity / 4f,
+                -body.linearVelocity.y * currentDensity / 4f,
+                true)
+        if (body.fixtureList.first().testPoint(inputController.x, inputController.y)) {
+            return
+        }
         val angle = MathUtils.atan2(inputController.y - body.position.y, inputController.x - body.position.x)
         val xForce = MathUtils.cos(angle);
         val yForce = MathUtils.sin(angle);
-        body.applyForceToCenter(xForce * (75 + size * 10), yForce * (75 + size * 10), true)
-        println(size)
+        body.applyForceToCenter(
+                xForce * currentDensity,
+                yForce * currentDensity,
+                true)
     }
 
     fun enlarge() {
-        size++
-        body.fixtureList.first().shape.radius += 1
+        size += 0.2f
+        body.fixtureList.first().shape.radius = size
     }
 }
