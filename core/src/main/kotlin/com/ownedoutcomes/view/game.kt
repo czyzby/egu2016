c82a0dcaec2168a119b9afa1a276984438d1b30c
Krzysiek Ruczaj <krj.public@gmail.com>
2016-11-19 18:00:02 +0100

    [UI] removed play button from game screen

--- core/src/main/kotlin/com/ownedoutcomes/view/game.kt
+++ core/src/main/kotlin/com/ownedoutcomes/view/game.kt
@@ -1,39 +1,34 @@
 package com.ownedoutcomes.view
 
-import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.utils.Align
 import com.ownedoutcomes.logic.GameController
 import com.ownedoutcomes.logic.GameRenderer
 import ktx.actors.onKey
-import ktx.scene2d.*
+import ktx.scene2d.KTableWidget
+import ktx.scene2d.table
 
 class Game(stage: Stage, private val gameController: GameController, val gameRenderer: GameRenderer) : AbstractView(stage) {
-    lateinit var image: Actor
-
     override val root = table {
         setFillParent(true)
         table { cell ->
             cell.expand().align(Align.bottom)
-            image = image("play-button") {
-                onKey { input: InputEvent, image: Image, c: Char ->
-                    run {
-                        if (c == ' ')
-                            gameController.players.forEach { it.enlarge() }
-                        if (c == '1')
-                            gameController.players.forEach { it.smaller() }
-                    }
-                }
-            }
         }
 
+        onKey { inputEvent: InputEvent, kTableWidget: KTableWidget, c: Char ->
+            run {
+                if (c == ' ')
+                    gameController.players.forEach { it.enlarge() }
+                if (c == '1')
+                    gameController.players.forEach { it.smaller() }
+            }
+        }
     }
 
     override fun show() {
         super.show()
-        stage.keyboardFocus = image // TODO remove
+        stage.keyboardFocus = root
     }
 
     override fun hide() {
